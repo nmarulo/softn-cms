8fe8f2a573c761fdda6690b00d2c1eecd61f689c
Nicolás Marulanda P <nicomaru@outlook.com>
2018-03-18 01:28:10 +0100

    Agregado script para mostrar y ocultar menu lateral

---
+++ public/assets/js/common.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#btn-navbar-menu-toggle').click(function () {
+        navbarToggle();
+    });
+});
+
+function navbarToggle() {
+    $('#navbar-collapse-fixed-top').toggleClass('toggle');
+    $('#main-container').toggleClass('toggle');
+}
