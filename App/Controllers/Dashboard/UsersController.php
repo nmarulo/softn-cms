916847605c1142a2e437100bb1e1ee0823f28560
Nicolás Marulanda P <nicomaru@outlook.com>
2018-07-15 01:13:21 +0200

    Formulario de búsqueda en tablas - Implementación - Clases - [Nuevo] [Clase:ModalHelper] Método "getTotalNumDataSearchModel". - [Modificado] [Clase:ModalHelper] Ahora el método "model", el segundo parámetro estable si debe inicializar la clase "SearchModel". Otros cambios varios.

--- App/Controllers/Dashboard/UsersController.php
+++ App/Controllers/Dashboard/UsersController.php
@@ -17,10 +17,10 @@ use Silver\Http\View;
 class UsersController extends Controller {
     
     public function index() {
-        $userModel = ModelFacade::model(Users::class)
-                                ->pagination()
-                                ->sort();
-        $users     = $userModel->all();
+        $userModel   = ModelFacade::model(Users::class, true)
+                                  ->pagination()
+                                  ->sort();
+        $users       = $userModel->all();
         
         return View::make('dashboard.users.index')
                    ->with('users', $users)
--- App/Helpers/ModelHelper.php
+++ App/Helpers/ModelHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Helpers;
 
+use App\Facades\SearchModelFacade;
 use Silver\Core\Bootstrap\Facades\Request;
 use Silver\Database\Model;
 use Silver\Database\Query;
@@ -21,69 +22,102 @@ class ModelHelper {
     /** @var Model */
     private $model;
     
+    /** @var SearchModelHelper */
+    private $searchModel;
+    
     /**
      * ViewHelper constructor.
      */
     public function __construct() {
-        $this->query      = NULL;
-        $this->pagination = NULL;
-        $this->model      = NULL;
+        $this->query       = NULL;
+        $this->pagination  = NULL;
+        $this->model       = NULL;
+        $this->searchModel = NULL;
     }
     
     /**
-     * @param Model $model
+     * @param Model   $model
+     * @param boolean $searchModel
      *
      * @return $this
      */
-    public function model($model) {
+    public function model($model, $searchModel = FALSE) {
         $this->model = $model;
+        
+        if ($searchModel) {
+            $this->searchModel = SearchModelFacade::model($model);
+            $this->query       = $this->searchModel->getQuery();
+        }
         
         return $this;
     }
     
     /**
      * @param \Closure $dataModelClosure
-     * @param int      $count
      *
      * @return $this
      */
-    public function pagination($dataModelClosure = NULL, $count = NULL) {
-        $this->pagination = $this->getPaginationInstance($count);
-        $query            = $this->getQuery();
-        $query            = $this->getDataModel($query, $dataModelClosure);
-        
-        $this->setQuery($query);
+    public function pagination($dataModelClosure = NULL) {
+        $this->instancePagination();
+        $this->paginationDataClosure($this->getQuery(), $dataModelClosure);
         
         return $this;
     }
     
-    /**
-     * @param Model $currentModel
-     * @param int   $count
-     *
-     * @return mixed
-     */
-    private function getPaginationInstance($count = NULL) {
+    private function instancePagination() {
         $currentPage = 1;
-        $totalData   = $count;
+        $totalData   = $this->getTotalNumDataSearchModel();
         
         if (Request::ajax()) {
             $currentPage = Request::input('page', 1);
         }
         
-        if ($count == NULL) {
+        if ($totalData == NULL) {
             $totalData = Query::count()
                               ->from($this->getTableName())
                               ->single();
         }
         
-        return \App\Facades\Pagination::getInstance($currentPage, $totalData);
+        $this->pagination = \App\Facades\Pagination::getInstance($currentPage, $totalData);
+    }
+    
+    private function getTotalNumDataSearchModel() {
+        if (empty($this->searchModel)) {
+            return NULL;
+        }
+        
+        return $this->searchModel->getCount();
     }
     
     private function getTableName() {
         $model = $this->model;
         
         return $model::tableName();
+    }
+    
+    /**
+     * @param mixed    $query
+     * @param \Closure $dataModelClosure
+     */
+    private function paginationDataClosure($query, $dataModelClosure = NULL) {
+        $numberRowShow = $this->pagination->getNumberRowShow();
+        $beginRow      = $this->pagination->getBeginRow();
+        
+        if ($dataModelClosure == NULL || !is_callable($dataModelClosure)) {
+            $query = $query->limit($numberRowShow)
+                           ->offset($beginRow);
+        } else {
+            $query = $dataModelClosure($query, $numberRowShow, $beginRow);
+        }
+        
+        $this->setQuery($query);
+    }
+    
+    /**
+     * @param Query $query
+     */
+    private function setQuery($query) {
+        $this->query = $query;
     }
     
     /**
@@ -99,31 +133,6 @@ class ModelHelper {
         }
         
         return $this->query;
-    }
-    
-    /**
-     * @param mixed    $query
-     * @param \Closure $dataModelClosure
-     *
-     * @return Query
-     */
-    private function getDataModel($query, $dataModelClosure = NULL) {
-        $numberRowShow = $this->pagination->getNumberRowShow();
-        $beginRow      = $this->pagination->getBeginRow();
-        
-        if ($dataModelClosure == NULL || !is_callable($dataModelClosure)) {
-            return $query->limit($numberRowShow)
-                         ->offset($beginRow);
-        }
-        
-        return $dataModelClosure($query, $numberRowShow, $beginRow);
-    }
-    
-    /**
-     * @param Query $query
-     */
-    private function setQuery($query) {
-        $this->query = $query;
     }
     
     /**
@@ -147,22 +156,18 @@ class ModelHelper {
         $sortColumn = Request::input('sortColumn');
         $query      = $this->getQuery();
         
-        if (Request::ajax()) {
-            if (!empty($sortColumn)) {
-                $sortColumn = (array)json_decode($sortColumn);
-                
-                if (is_array($sortColumn)) {
+        if (Request::ajax() && !empty($sortColumn)) {
+            $sortColumn = (array)json_decode($sortColumn);
+            
+            if (is_array($sortColumn)) {
+                foreach ($sortColumn as $value) {
+                    $value = (array)$value;
                     
-                    foreach ($sortColumn as $value) {
-                        $value = (array)$value;
-                        
-                        if (empty($value['column']) || empty($value['sort'])) {
-                            continue;
-                        }
-                        
-                        $query = $query->orderBy($value['column'], $value['sort']);
+                    if (empty($value['column']) || empty($value['sort'])) {
+                        continue;
                     }
                     
+                    $query = $query->orderBy($value['column'], $value['sort']);
                 }
             }
         }
