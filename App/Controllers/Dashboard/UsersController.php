6a9621c793497418590ed8c2ab5a4ca911a00391
Nicolás Marulanda P <nicomaru@outlook.com>
2018-08-25 00:19:29 +0200

    UsersController - Actualizado según los cambios realizado a la "RequestApi"

--- App/Controllers/Dashboard/UsersController.php
+++ App/Controllers/Dashboard/UsersController.php
@@ -21,14 +21,22 @@ class UsersController extends Controller {
     private $urlUsers = 'dashboard/users';
     
     public function index() {
-        $request = Request::all();
+        $pagination = NULL;
+        $users      = [];
+        $request    = Request::all();
         //TODO: ERROR: solo al enviar, por get, "'uri' => '/dashboard/users'", por eso lo elimino, en caso de enviarlo.
         unset($request['uri']);
-        $result     = RequestApiFacade::makeGetRequest($request, $this->urlUsers);
-        $pagination = Pagination::arrayToObject($result['pagination']);
-        $users      = array_map(function($value) {
-            return ModelFacade::arrayToObject($value, Users::class);
-        }, $result['users']);
+        RequestApiFacade::makeGetRequest($request, $this->urlUsers);
+        
+        if (RequestApiFacade::isError()) {
+            Messages::addDanger(RequestApiFacade::getMessageError());
+        } else {
+            $response   = RequestApiFacade::getResponse();
+            $pagination = Pagination::arrayToObject($response['pagination']);
+            $users      = array_map(function($value) {
+                return ModelFacade::arrayToObject($value, Users::class);
+            }, $response['users']);
+        }
         
         return View::make('dashboard.users.index')
                    ->with('users', $users)
@@ -36,30 +44,41 @@ class UsersController extends Controller {
     }
     
     public function form($id) {
+        $user = new Users();
+        
         if (Utils::isPostRequest()) {
             $message = 'Usuario actualizado correctamente.';
             $request = Request::all();
+            unset($request['uri']);
             
             if (empty($id)) {
                 $message = 'Usuario creado correctamente';
-                $user    = RequestApiFacade::makePostRequest($request, $this->urlUsers);
+                RequestApiFacade::makePostRequest($request, $this->urlUsers);
             } else {
                 $request['id'] = $id;
-                $user          = RequestApiFacade::makePutRequest($request, $this->urlUsers);
+                RequestApiFacade::makePutRequest($request, $this->urlUsers);
             }
             
-            $user = ModelFacade::arrayToObject($user, Users::class);
-            Messages::addSuccess($message);
             
-            if (empty($id)) {
-                Redirect::to(sprintf('%1$s/%2$s/form/%3$s', URL, $this->urlUsers, $user->id));
+            if (RequestApiFacade::isError()) {
+                Messages::addDanger(RequestApiFacade::getMessageError());
+                $user = ModelFacade::arrayToObject($request, Users::class);
+            } else {
+                Messages::addSuccess($message);
+                $user = ModelFacade::arrayToObject(RequestApiFacade::getResponse(), Users::class);
+                
+                if (empty($id)) {
+                    Redirect::to(sprintf('%1$s/%2$s/form/%3$s', URL, $this->urlUsers, $user->id));
+                }
             }
         } elseif ($id) {
-            $user = RequestApiFacade::makeGetRequest('', $this->urlUsers, $id);
-            //TODO: comprobar el código de respuesta http
-            $user = ModelFacade::arrayToObject($user, Users::class);
-        } else {
-            $user = new Users();
+            RequestApiFacade::makeGetRequest('', $this->urlUsers, $id);
+            
+            if (RequestApiFacade::isError()) {
+                Messages::addDanger(RequestApiFacade::getMessageError());
+            } else {
+                $user = ModelFacade::arrayToObject(RequestApiFacade::getResponse(), Users::class);
+            }
         }
         
         return View::make('dashboard.users.form')
@@ -67,10 +86,12 @@ class UsersController extends Controller {
     }
     
     public function delete() {
-        if (RequestApiFacade::makeDeleteRequest(Request::all(), $this->urlUsers)) {
-            Messages::addSuccess('Usuario borrado correctamente.');
+        RequestApiFacade::makeDeleteRequest(Request::all(), $this->urlUsers);
+        
+        if (RequestApiFacade::isError()) {
+            Messages::addDanger(RequestApiFacade::getMessageError());
         } else {
-            Messages::addDanger('El usuario no existe.');
+            Messages::addSuccess('Usuario borrado correctamente.');
         }
     }
 }
