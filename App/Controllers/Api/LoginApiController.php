ec0510d1c66238ec34d35b3cce7ae9d2165f0106
Nicolás Marulanda P <nicomaru@outlook.com>
2018-08-25 13:52:50 +0200

    API - Se elimino la implementación "ResponseApi", ahora esta se realiza en la clase "ApiMiddleware".

--- App/Controllers/Api/LoginApiController.php
+++ App/Controllers/Api/LoginApiController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers\Api;
 
-use App\Facades\Api\ResponseApiFacade;
 use App\Facades\TokenFacade;
 use App\Models\Users;
 use Lcobucci\JWT\Builder;
+use Silver\Core\Bootstrap\Facades\Request;
 use Silver\Core\Controller;
 
 /**
@@ -14,28 +14,26 @@ use Silver\Core\Controller;
 class LoginApiController extends Controller {
     
     public function login() {
-        return ResponseApiFacade::makeResponse(function($request) {
-            $user = Users::where('user_login', '=', $request['user_login'])
-                         ->first();
-            
-            //Si el usuario existe y su contraseña es igual
-            if ($user && $this->checkPassword($user, $request)) {
-                TokenFacade::generate(function(Builder $builder) use ($user) {
-                    $builder->set('user_login', $user->user_login);
-                    
-                    return $builder;
-                });
+        $user = Users::where('user_login', '=', Request::input('user_login'))
+                     ->first();
+        
+        //Si el usuario existe y su contraseña es igual
+        if ($user && $this->checkPassword($user)) {
+            TokenFacade::generate(function(Builder $builder) use ($user) {
+                $builder->set('user_login', $user->user_login);
                 
-                return $user;
-            } else {
-                throw new \RuntimeException('Usuario y/o contraseña incorrecto(s).');
-            }
-        });
+                return $builder;
+            });
+            
+            return $user;
+        } else {
+            throw new \RuntimeException('Usuario y/o contraseña incorrecto(s).');
+        }
     }
     
-    private function checkPassword($user, $request) {
+    private function checkPassword($user) {
         //TODO: cifrar
-        return $user->user_password == $request['user_password'];
+        return $user->user_password == Request::input('user_password');
     }
     
 }
