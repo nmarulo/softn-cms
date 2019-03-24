f7a23f445dac2724ce603924828a39994760958b
Nicolás Marulanda P <nicomaru@outlook.com>
2019-03-24 13:56:14 +0100

    UsersApiController - Petición GET - Refactorizado para el uso de la clase UserRequest.

--- App/Controllers/Api/Dashboard/UsersApiController.php
+++ App/Controllers/Api/Dashboard/UsersApiController.php
@@ -5,6 +5,8 @@ namespace App\Controllers\Api\Dashboard;
 use App\Facades\ModelFacade;
 use App\Facades\Utils;
 use App\Models\Users;
+use App\Rest\Request\UserRequest;
+use App\Rest\Response\UserResponse;
 use Silver\Core\Bootstrap\Facades\Request;
 use Silver\Core\Controller;
 
@@ -14,32 +16,32 @@ use Silver\Core\Controller;
 class UsersApiController extends Controller {
     
     public function get($id) {
+        $userResponse = new UserResponse();
+        
         if ($id) {
-            return $this->getUserById($id);
+            $userResponse->users = $this->userModelToDTO($this->getUserById($id));
+            
+            return $userResponse->toArray();
         }
         
-        $userModel = ModelFacade::model(Users::class)
+        //TODO: Hasta que no encuentre una forma de capturar la instancia del controlador desde el middleware tendré que seguir usando la clase "Request".
+        $request = Utils::parseOf(Request::all(), UserRequest::class);
+        
+        $userModel = ModelFacade::model(Users::class, $request->dataTable)
                                 ->search()
                                 ->pagination()
                                 ->sort();
         
-        return [
-                'users'      => $userModel->all(),
-                'pagination' => $userModel->getPagination(),
-        ];
-    }
-    
-    /**
-     * @param $id
-     *
-     * @return Users
-     */
-    private function getUserById($id) {
-        if ($user = Users::find($id)) {
-            return $user;
-        }
+        $users = $userModel->all();
         
-        throw new \RuntimeException("Usuario desconocido.");
+        $usersDTO = array_map(function(Users $user) {
+            return $this->userModelToDTO($user);
+        }, $users);
+        
+        $userResponse->users      = $usersDTO;
+        $userResponse->pagination = $userModel->getPagination();
+        
+        return $userResponse->toArray();
     }
     
     public function post() {
@@ -70,5 +72,29 @@ class UsersApiController extends Controller {
     public function delete() {
         $user = $this->getUserById(Request::input('id'));
         $user->delete();
+    }
+    
+    private function userModelToDTO(Users $user): \App\Rest\Dto\Users {
+        $userDTO                 = new \App\Rest\Dto\Users();
+        $userDTO->id             = $user->id;
+        $userDTO->userLogin      = $user->user_login;
+        $userDTO->userEmail      = $user->user_email;
+        $userDTO->userName       = $user->user_name;
+        $userDTO->userRegistered = $user->user_registered;
+        
+        return $userDTO;
+    }
+    
+    /**
+     * @param $id
+     *
+     * @return Users
+     */
+    private function getUserById($id) {
+        if ($user = Users::find($id)) {
+            return $user;
+        }
+        
+        throw new \RuntimeException("Usuario desconocido.");
     }
 }
